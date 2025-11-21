3de571683d532b69b171ec9d9fa0259d678b882d
Maël Cuny <maelpro@MacBook-Air-de-Mael.local>
2025-11-21 11:45:31 +0100

    modified Login.jsx for task 1

--- react_props/task_1/dashboard/src/Login/Login.jsx
+++ react_props/task_1/dashboard/src/Login/Login.jsx
@@ -1,13 +1,23 @@
+import { useRef } from 'react';
 import './Login.css';
 
 function Login() {
+    const emailInputRef = useRef(null);
+    const passwordInputRef = useRef(null);
+
+    const focusInput = (inputRef) => () => {
+        if (inputRef.current) {
+            inputRef.current.focus();
+        }
+    };
+
     return (
         <div className="App-login">
             <p>Login to access the full dashboard</p>
-            <label htmlFor="email">Email</label>
-            <input type="email" name="user_email" id="email" />
-            <label htmlFor="password">Password</label>
-            <input type="text" name="user_password" id="password" />
+            <label htmlFor="email" onClick={focusInput(emailInputRef)}>Email</label>
+            <input type="email" name="user_email" id="email" ref={emailInputRef} />
+            <label htmlFor="password" onClick={focusInput(passwordInputRef)}>Password</label>
+            <input type="password" name="user_password" id="password" ref={passwordInputRef} />
             <button role="button" type="submit">OK</button>
         </div>
     );
