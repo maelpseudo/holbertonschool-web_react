177836a614cf38e7c53c674d0447e12347b900e2
Maël Cuny <maelpro@MacBook-Air-de-Mael.local>
2025-11-25 11:48:23 +0100

    Added react styling task 0

---
+++ react_styling/task_0/dashboard/src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import './main.css';
+import App from './App/App';
+import './App/App.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
